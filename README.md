dedeb1091aee7fb18c4bdb9a16ecbf3d9bd670a0
Edmundo Rubio <zerphyr.r@gmail.com>
2018-04-01 15:03:31 -0600

    1rst Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Quick Node.js HTML
+
+This is the example of a quick Node.js server, serving HTML pages. 
+
---
+++ server.js
@@ -0,0 +1,29 @@
+// Loading Dependencies
+// ====================================================
+const app    = require('express')()
+const morgan = require('morgan')
+const path   = require('path')
+
+
+// Using Dependencies
+// ====================================================
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/pages/index.html'))
+})
+
+app.get('/1', (req, res) => {
+    res.sendFile(path.join(__dirname + '/pages/about.html'))
+})
+
+
+// Server Listener
+// ====================================================
+app.listen((process.env.PORT || 5000), (err) => {
+    if(err){ throw err }
+    console.log(" ================================== \n Server Running... \n ")
+})
+
+
+
